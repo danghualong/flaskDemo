2818803631f5c9135ba6019962b0fba27b5b84b4
danghualong <dhl_345@qq.com>
2020-02-18 16:54:44 +0800

    filename属性拼写错误

--- facerecognizer/fileUtil.py
+++ facerecognizer/fileUtil.py
@@ -6,7 +6,7 @@ FILE_TMP_PATH='facerecognizer\\tmpimgs'
 def saveFile(imgFile):
     if(not os.path.exists(FILE_TMP_PATH)):
         os.makedirs(FILE_TMP_PATH)
-    oldName=imgFile.fileName
+    oldName=imgFile.filename
     ext=os.path.splitext(oldName)[1]
     fileName='img_'+str(uuid.uuid1()).replace('-','')+ext
     newFilePath=os.path.join(FILE_TMP_PATH,fileName)
