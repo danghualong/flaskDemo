aef2ea485d42adf54d28d9887e29880d6f5d7f08
danghualong <dhl_345@qq.com>
2020-02-18 16:46:57 +0800

    添加文件后缀

--- facerecognizer/fileUtil.py
+++ facerecognizer/fileUtil.py
@@ -6,7 +6,9 @@ FILE_TMP_PATH='facerecognizer\\tmpimgs'
 def saveFile(imgFile):
     if(not os.path.exists(FILE_TMP_PATH)):
         os.makedirs(FILE_TMP_PATH)
-    fileName='img_'+str(uuid.uuid1()).replace('-','')
+    oldName=imgFile.fileName
+    ext=os.path.splitext(oldName)[1]
+    fileName='img_'+str(uuid.uuid1()).replace('-','')+ext
     newFilePath=os.path.join(FILE_TMP_PATH,fileName)
     imgFile.save(newFilePath)
     return fileName
