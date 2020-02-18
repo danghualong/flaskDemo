9b5c65cde959d472be65279ad1a99a4aa5c53d24
danghualong <dhl_345@qq.com>
2020-02-18 15:33:44 +0800

    增加文件上传接口

--- facerecognizer/blueprints/reco.py
+++ facerecognizer/blueprints/reco.py
@@ -6,14 +6,25 @@ import facerecognizer.fileUtil as fileUtil
 
 reco_bp=Blueprint('reco',__name__)
 
-@reco_bp.route('/identity',methods=['POST'])
-def checkIn():
+@reco_bp.route('/uploadimg',methods=['POST'])
+def uploadimg():
     try:
         file = request.files.get('upload')
         if file:
             # save file to tmp file
             filePath=fileUtil.saveFile(file)
-            persons=reco.recognize(filePath)
+            return jsonify({'filePath':filePath})
+        else:
+            return jsonify({'error':'没有上传图片','code':601})
+    except Exception as ex:
+        print(ex.args)
+        return jsonify({'error':'上传参数有误','code':901})
+
+@reco_bp.route('/identity/<imgpath>',methods=['POST'])
+def checkIn(imgpath):
+    try:
+        if imgPath:
+            persons=reco.recognize(imgPath)
             if(persons==None):
                 return jsonify({'error':'inner error','code':702})    
             return jsonify(persons)
